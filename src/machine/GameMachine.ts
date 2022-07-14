e69eae3ecd32437c398688a73f74e7fe5e0a36f8
RominM <rominmanogil.off@gmail.com>
2022-07-14 17:23:05 +0200

    feat: :construction: add GameModel

--- src/machine/GameMachine.ts
+++ src/machine/GameMachine.ts
@@ -1,4 +1,8 @@
 import { createMachine } from "xstate";
+import { createModel } from "xstate/lib/model";
+import { GridState, Player, PlayerColor } from "../types";
+import { canJoinGuards } from "./Guards";
+
 
 enum GameStates {
   LOBBY = 'LOBBY',
@@ -7,13 +11,39 @@ enum GameStates {
   DRAW = 'DRAW'
 }
 
-export const GameMachine = createMachine({
+// Not imperatf to use createModel with XState but, that give you a caring type with TypeScipt => check "events"
+export const GameModel = createModel({
+  players: [] as Player[],
+  currentPlayer: null as null | Player['id'],
+  tokenWinLength: 4,
+  grid: [
+    ['E','E','E','E','E','E','E'],
+    ['E','E','E','E','E','E','E'],
+    ['E','E','E','E','E','E','E'],
+    ['E','E','E','E','E','E','E'],
+    ['E','E','E','E','E','E','E'],
+    ['E','E','E','E','E','E','E']
+  ] as GridState
+}, {
+  events: {
+    join: (playerId: Player['id'], name: Player['name']) => ({playerId, name}),
+    leave: (playerId: Player['id']) => ({playerId}),
+    chooseColor: (playerId: Player['id'], color: PlayerColor) => ({playerId, color}),
+    start: (playerId: Player['id']) => ({playerId}),
+    dropToken: (playerId: Player['id'], xPos: number) => ({playerId, xPos}),
+    restart: (playerId: Player['id']) => ({playerId}),
+  }
+});
+
+export const GameMachine = GameModel.createMachine({
   id: 'game',
+  context: GameModel.initialContext,
   initial: GameStates.LOBBY,
   states: {
     [GameStates.LOBBY]: {
       on: {
         join: { 
+          cond: canJoinGuards,
           target: GameStates.LOBBY
         },
         leave: {
